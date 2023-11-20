0b0b03cd6a5e32370e029f175caf5fad9c4638c2
Zoltan Csizmadia <Zoltan.Csizmadia@vericast.com>
2023-11-20 14:27:43 -0600

    Debug

--- build.sh
+++ build.sh
@@ -7,14 +7,13 @@ apt-get update -q
 apt-get install -q -y --no-install-recommends curl libzstd-dev libicu-dev git ca-certificates
 
 # Install .NET SDKs
-curl --insecure --output /tmp/dotnet-install.sh https://dot.net/v1/dotnet-install.sh
-chmod +x /tmp/dotnet-install.sh
+curl --insecure https://dot.net/v1/dotnet-install.sh
 
-/tmp/dotnet-install.sh --channel "3.1" --install-dir "/usr/share/dotnet" # 3.1
-/tmp/dotnet-install.sh --channel "5.0" --install-dir "/usr/share/dotnet" # 5.0
-/tmp/dotnet-install.sh --channel "6.0" --install-dir "/usr/share/dotnet" # 6.0
-/tmp/dotnet-install.sh --channel "7.0" --install-dir "/usr/share/dotnet" # 7.0
-/tmp/dotnet-install.sh --channel "7.0" --install-dir "/usr/share/dotnet" # 8.0
+bash ./dotnet-install.sh --channel "3.1" --install-dir "/usr/share/dotnet" # 3.1
+bash ./dotnet-install.sh --channel "5.0" --install-dir "/usr/share/dotnet" # 5.0
+bash ./dotnet-install.sh --channel "6.0" --install-dir "/usr/share/dotnet" # 6.0
+bash ./dotnet-install.sh --channel "7.0" --install-dir "/usr/share/dotnet" # 7.0
+bash ./dotnet-install.sh --channel "8.0" --install-dir "/usr/share/dotnet" # 8.0
 
 export PATH=$PATH:/usr/share/dotnet
 
