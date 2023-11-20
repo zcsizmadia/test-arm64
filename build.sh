4c799f298fc8c2bef33b6e217148e5a71ed78dfc
Zoltan Csizmadia <Zoltan.Csizmadia@vericast.com>
2023-11-20 14:10:49 -0600

    Debug

--- build.sh
+++ build.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 apt-get update -q
-apt-get install -q -y --no-install-recommends wget libzstd-dev libicu-dev git
-wget https://dot.net/v1/dotnet-install.sh
-bash ./dotnet-install.sh --channel "3.1" --install-dir "/usr/share/dotnet" # 3.1
-bash ./dotnet-install.sh --channel "5.0" --install-dir "/usr/share/dotnet" # 5.0
-bash ./dotnet-install.sh --channel "6.0" --install-dir "/usr/share/dotnet" # 6.0
-bash ./dotnet-install.sh --channel "7.0" --install-dir "/usr/share/dotnet" # 7.0
-bash ./dotnet-install.sh --channel "7.0" --install-dir "/usr/share/dotnet" # 8.0
+apt-get install -q -y --no-install-recommends curl libzstd-dev libicu-dev git ca-certificates
+
+curl --insecure --output /tmp/dotnet-install.sh https://dot.net/v1/dotnet-install.sh
+chmod +x /tmp/dotnet-install.sh
+
+/tmp/dotnet-install.sh --channel "3.1" --install-dir "/usr/share/dotnet" # 3.1
+/tmp/dotnet-install.sh --channel "5.0" --install-dir "/usr/share/dotnet" # 5.0
+/tmp/dotnet-install.sh --channel "6.0" --install-dir "/usr/share/dotnet" # 6.0
+/tmp/dotnet-install.sh --channel "7.0" --install-dir "/usr/share/dotnet" # 7.0
+/tmp/dotnet-install.sh --channel "7.0" --install-dir "/usr/share/dotnet" # 8.0
 
 export PATH=$PATH:/usr/share/dotnet
 
@@ -17,4 +20,3 @@ ls -al
 
 git config --global url.https://github.com/.insteadOf git://github.com/
 git clone https://github.com/githubtraining/hellogitworld.git
-
