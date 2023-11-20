896ef3c382bdc4bc5d4b85956bddb250268c38a9
Zoltan Csizmadia <Zoltan.Csizmadia@vericast.com>
2023-11-20 16:00:10 -0600

    Debug

--- build.sh
+++ build.sh
@@ -4,15 +4,14 @@ set -e
 set -x
 
 apt-get update -q
-apt-get install -q -y --no-install-recommends wget libzstd-dev libicu-dev git ca-certificates
+apt-get install -q -y --no-install-recommends curl libzstd-dev libicu-dev ca-certificates
 
 # Install .NET SDKs
-wget --no-check-certificate https://dot.net/v1/dotnet-install.sh
-
-bash ./dotnet-install.sh --channel "8.0" --install-dir "/usr/share/dotnet" # 8.0
-
+curl -sSLk --output - https://dot.net/v1/dotnet-install.sh |
+bash /dev/stdin --channel "8.0" --install-dir "/usr/share/dotnet" # 8.0
 export PATH=$PATH:/usr/share/dotnet
 
+# Build and run
 pushd src
 dotnet build -c Release
 dotnet run -c Release
