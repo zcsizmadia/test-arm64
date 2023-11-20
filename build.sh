26b92bd5354d2e9b56f35cc45f7524885158fef5
Zoltan Csizmadia <Zoltan.Csizmadia@vericast.com>
2023-11-20 15:31:51 -0600

    Debug

--- build.sh
+++ build.sh
@@ -9,19 +9,11 @@ apt-get install -q -y --no-install-recommends curl libzstd-dev libicu-dev git ca
 # Install .NET SDKs
 curl --insecure https://dot.net/v1/dotnet-install.sh
 
-bash ./dotnet-install.sh --channel "3.1" --install-dir "/usr/share/dotnet" # 3.1
-bash ./dotnet-install.sh --channel "5.0" --install-dir "/usr/share/dotnet" # 5.0
-bash ./dotnet-install.sh --channel "6.0" --install-dir "/usr/share/dotnet" # 6.0
-bash ./dotnet-install.sh --channel "7.0" --install-dir "/usr/share/dotnet" # 7.0
 bash ./dotnet-install.sh --channel "8.0" --install-dir "/usr/share/dotnet" # 8.0
 
 export PATH=$PATH:/usr/share/dotnet
 
-# Clone repo
-git config --global url.https://github.com/.insteadOf git://github.com/
-git clone https://github.com/ied206/Joveler.Compression.git
-
-pushd Joveler.Compression
+pushd src
 dotnet build -c Release
-dotnet test -c Release
+dotnet run -c Release
 popd
---
+++ src/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using Joveler.Compression.XZ;
+
+namespace Test
+{
+    class Program
+    {
+        static void InitializeXZ()
+        {
+            string arch = RuntimeInformation.OSArchitecture.ToString().ToLower();
+            string foundLibPath = string.Empty;
+            string libPath;
+            string rid;
+            string libName;
+
+            // Determine Platform (needed for proper Runtime ID)
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                rid = $"win-{arch}";
+                libName = "liblzma.dll";
+            }
+            else
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                rid = $"linux-{arch}";
+                libName = "liblzma.so";
+            }
+            else
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                rid = $"osx-{arch}";
+                libName = "liblzma.dylib";
+            }
+            else
+            {
+                // Unknown platform
+                throw new PlatformNotSupportedException("Unknown runtime platform!");
+            }
+
+            // Try to search for the lib in the working directory and the application binary directory
+            foreach (var relPath in new List<string> { ".", AppDomain.CurrentDomain.BaseDirectory })
+            {
+                // Try first the lib name directly
+                libPath = Path.Combine(relPath, libName);
+                if (System.IO.File.Exists(libPath))
+                {
+                    foundLibPath = libPath;
+                    break;
+                }
+
+                // Try the runtimes/RID/native location
+                // This is the default location for netstandard native libs
+                libPath = Path.Combine(relPath, "runtimes", rid, "native", libName);
+                if (System.IO.File.Exists(libPath))
+                {
+                    foundLibPath = libPath;
+                    break;
+                }
+            }
+
+            // Try the OS search path if nothing is found yet
+            if (string.IsNullOrEmpty(foundLibPath))
+            {
+                var values = Environment.GetEnvironmentVariable("PATH");
+                if (!string.IsNullOrEmpty(values))
+                {
+                    foreach (string path in values.Split(Path.PathSeparator))
+                    {
+                        libPath = Path.Combine(path, libName);
+                        if (System.IO.File.Exists(libPath))
+                        {
+                            foundLibPath = libPath;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (string.IsNullOrEmpty(foundLibPath))
+                throw new PlatformNotSupportedException($"Unable to find {libName}");
+
+            // Initialize XZ library
+            XZInit.GlobalInit(foundLibPath);
+        }
+
+        static void Main(string[] args)
+        {
+            InitializeXZ();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
